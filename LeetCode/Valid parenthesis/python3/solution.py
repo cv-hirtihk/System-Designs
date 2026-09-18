3616a896dfc0702498aa73786191e6a5abadbf92
Hirtih Kumar C V <61385197+cv-hirtihk@users.noreply.github.com>
2026-09-18 07:55:30 +0530

    Add isValid function for parentheses validation
    Implement a function to check for valid parentheses in a string.

---
+++ LeetCode/Valid parenthesis/python3/solution.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        data = {')':'(', '}':'{', ']':'['}
+
+        for i in s:
+            if i in ['(', '{', '[']:
+                stack.append(i)
+            elif (len(stack) == 0) or (data[i] != stack.pop()):
+                return False
+        if stack == []:
+            return True
+        else:
+            return False
+
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# An input string is valid if:
+#     Open brackets must be closed by the same type of brackets.
+#     Open brackets must be closed in the correct order.
+#     Every close bracket has a corresponding open bracket of the same type.
+
+# Example 1:
+# Input: s = "()"
+# Output: true
+
+# Example 2:
+# Input: s = "()[]{}"
+# Output: true
+
+# Example 3:
+# Input: s = "(]"
+# Output: false
+
+# Example 4:
+# Input: s = "([])"
+# Output: true
+
+# Example 5:
+# Input: s = "([)]"
+# Output: false
